a3bfaa92fbdb5829d17d4d8c04439d57de154e2f
VT <chickenstills+x@gmail.com>
2025-05-11 17:32:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,12 @@ setup(
     install_requires=open("requirements.txt").read().splitlines(),
     author="VT",
     description="A lovense control link library to connect to lovense links",
+    license='GPLv3',
+    license_files=['LICENSE'],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10.9',
 )
